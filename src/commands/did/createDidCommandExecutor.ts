3c4e71898e6ab720add3428931b8bb12a400c771
Jose Manuel Cantera <jose.cantera@iota.org>
2021-04-05 16:42:30 +0200

    First version of DID creation

--- src/commands/did/createDidCommandExecutor.ts
+++ src/commands/did/createDidCommandExecutor.ts
@@ -1,7 +1,30 @@
+/* eslint-disable no-duplicate-imports */
+
+import { Document, KeyType, publish as iotaDidPublish } from "@iota/identity-wasm/node";
+import type { NewDocument } from "@iota/identity-wasm/node";
+import { inspect } from "util";
 import { Arguments } from "yargs";
 
 export default class CreateDidCommandExecutor {
   public static async execute(args: Arguments): Promise<boolean> {
+    const { doc, key } = (new Document(KeyType.Ed25519) as unknown) as NewDocument;
+
+    doc.sign(key);
+
+    const transactionId = await iotaDidPublish(doc, {
+      network: "main"
+    });
+
+    console.log(inspect({
+      did: doc.toJSON().id,
+      keys: {
+        public: key.public,
+        private: key.secret
+      },
+      transactionUrl: `https://explorer.iota.org/mainnet/transaction/${transactionId}`
+    }, { showHidden: false, depth: Infinity, colors: true })
+    );
+
     return true;
   }
 }
